ffc84e8dfd6b58a34156597fe91c18f9da6ec263
JIS0098 <llqrw55@naver.com>
2023-05-17 19:40:05 +0900

    리뷰 부분 맵함수 오류

--- src/page/MovieDetail.js
+++ src/page/MovieDetail.js
@@ -8,21 +8,28 @@ import api from "../redux/api";
 const API_KEY = process.env.REACT_APP_API_KEY;
 
 const MovieDetail = () => {
+  // 장르 데이타 가져오기
+  // 장르 
   const { id } = useParams();
-  const [movie, setMovie] = useState({});
+  const [movie, setMovie] = useState([]);
+  const [reviews ,setReviews] = useState([]);
   const getMoviesDetail = async () => {
     let detailApi = await api.get(`/movie/${id}?api_key=${API_KEY}&language=en-US`);
-    let detailData =detailApi.data;
+    let detailData = detailApi.data;
+    let reviewsApi = await api.get(`/movie/${id}/reviews?api_key=${API_KEY}&language=en-US`);
+    let reviews = reviewsApi.data
     setMovie(detailData)
+    setReviews(reviews)
   };
-  
-  console.log(movie)
+
+  console.log(movie.genres)
+  console.log(reviews)
   useEffect(() => {
     getMoviesDetail();
   }, []);
 
   return (
-    <div>
+    <div className="detail-area">
       <section
         className="detail-banner"
         style={{
@@ -46,44 +53,76 @@ const MovieDetail = () => {
           </Row>
         </Container>
       </section>
-      <Container>
-        <Row>
-          <Col lg={6}>
-            <img src={`https://www.themoviedb.org/t/p/w300_and_h450_bestv2/${movie.poster_path}`} />
-          </Col>
-          <Col lg={6}>
-            <Badge bg="danger">zz</Badge>
-            <div>{movie?.title}</div>
-            <ul>
-              <li>{movie?.vote_average}</li>
-              <li>{movie?.popularity}</li>
-              <li>{movie.adult? "":"Under 18"}</li>
-            </ul>
-            <div>{movie?.overview}</div>
-            <div className="movie-info-list">
-              <ul>
-                <li>
-                <Badge bg="danger">zz</Badge>
-                <div>{movie?.budget}</div>
-                </li>
-                <li>
-                <Badge bg="danger">zz</Badge>
-                <div>{movie?.revenue}</div>
-                </li>
-                <li>
-                <Badge bg="danger">zz</Badge>
-                <div>{movie?.release_date}</div>
-                </li>
-                <li>
-                <Badge bg="danger">zz</Badge>
-                <div>{movie?.runtime}</div>
-                </li>
-              </ul>
-            </div>
-          </Col>
-        </Row>
-      </Container>
-      <section></section>
+      <section >
+        <Container className="container">
+          <Row className="screen-top">
+            <Col className="movie-left-page" lg={6}>
+              <img src={`https://www.themoviedb.org/t/p/original/${movie.poster_path}`} />
+            </Col>
+            <Col className="movie-right-page" lg={6}>
+              <div className="movie-area" >
+                <ul className="movie-genres">
+                  {movie.genres && movie.genres.map((item) => (<li><Badge className="badge" bg="danger"><div>{item.name}</div></Badge></li>))}
+                </ul>
+                <h1>{movie?.title}</h1>
+                <div className="movie-info">
+                  <ul className="info1">
+                    <li>{movie?.vote_average}</li>
+                    <li>{movie?.popularity}</li>
+                    <li>{movie.adult ? "" : "Under 18"}</li>
+                  </ul>
+                  <div className="white-line">
+                    <div className="movie-overview"><p>{movie?.overview}</p></div>
+                    <div className="info2">
+                      <ul className="info2-list">
+                        <li>
+                          <Badge bg="danger"><div>Budget</div></Badge>
+                          <div>${movie?.budget}</div>
+                        </li>
+                        <li>
+                          <Badge bg="danger"><div>Revenue</div></Badge>
+                          <div>${movie?.revenue}</div>
+                        </li>
+                        <li>
+                          <Badge bg="danger"><div>Release Day</div></Badge>
+                          <div>{movie?.release_date}</div>
+                        </li>
+                        <li>
+                          <Badge bg="danger"><div>Time</div></Badge>
+                          <div>{movie?.runtime}</div>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+
+                </div>
+                <div className="movie-warning">
+                  Watch Trailer
+                </div>
+                <div className="movie-like">
+                  ♥
+                </div>
+              </div>
+            </Col>
+          </Row>
+          <Row className="screen-bottom">
+            <Col lg={12}>
+              <div className="review-nav">
+                <button>REVIEWS (5)</button>
+                <button>RELATED MOVIES (20)</button>
+              </div>
+              <div className="review-list">
+                <ul> 
+                {/*reviews && reviews.results.map((review)=>(<li className="review"><h4>{review?.author}</h4><p>{review?.content}</p></li>))*/}
+                </ul>
+               
+              </div>
+
+            </Col>
+          </Row>
+        </Container>
+      </section>
+
     </div>
   );
 };
