0ab0832b6e1a072e5c39b7b7e20f4c602e374c36
JIS0098 <llqrw55@naver.com>
2023-05-24 19:27:30 +0900

    정렬 도전

--- src/page/Movies.js
+++ src/page/Movies.js
@@ -9,6 +9,9 @@ import "../css/movie.css";
 import Pagination from '../components/Pagination';
 import { useSearchParams } from 'react-router-dom';
 import api from '../redux/api';
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import ListGroup from 'react-bootstrap/ListGroup';
 
 
 
@@ -17,16 +20,16 @@ const API_KEY = process.env.REACT_APP_API_KEY;
 
 const Movies = () => {
   const { popularMovies, loading, } = useSelector((state) => state.movie);
-  const [query, setQuery]= useSearchParams("");
-  const [searchMovie , setSearchMovie]= useState({});
+  const [query, setQuery] = useSearchParams("");
+  const [searchMovie, setSearchMovie] = useState({});
   const dispatch = useDispatch();
   console.log("인기", popularMovies);
   let keyword = query.get("q")
-  const getSearchMovie=async()=>{
-    let searchMovieApi= await api.get(`/search/movie?api_key=${API_KEY}&language=en-US&page=1&include_adult=false&query=${keyword}`)
-    let searchMovie=searchMovieApi.data
+  const getSearchMovie = async () => {
+    let searchMovieApi = await api.get(`/search/movie?api_key=${API_KEY}&language=en-US&page=1&include_adult=false&query=${keyword}`)
+    let searchMovie = searchMovieApi.data
     setSearchMovie(searchMovie)
-    console.log("검색데이터",searchMovie)
+    console.log("검색데이터", searchMovie)
   }
 
 
@@ -44,7 +47,21 @@ const Movies = () => {
       <Row>
         <Col lg={4}>
           <div>
-            <div>Sort</div>
+            <DropdownButton id="dropdown-item-button" title="Sort">
+              <Dropdown.ItemText>Sort Results By</Dropdown.ItemText>
+              <DropdownButton id="dropdown-item-button" title="Sort By">
+                <Dropdown.Item as="button">None</Dropdown.Item>
+                <Dropdown.Item as="button">Popularity(Desc)</Dropdown.Item>
+                <Dropdown.Item as="button">Popularity(Asc)</Dropdown.Item>
+                <Dropdown.Item as="button">Release Day(Desc)</Dropdown.Item>
+                <Dropdown.Item as="button">Release Day(Asc)</Dropdown.Item>
+                <Dropdown.Item as="button">Vote(Desc)</Dropdown.Item>
+                <Dropdown.Item as="button">Vote(Asc)</Dropdown.Item>
+                <Dropdown.Item as="button">Revenue(Desc)</Dropdown.Item>
+                <Dropdown.Item as="button">Revenue(Asc)</Dropdown.Item>
+
+              </DropdownButton>
+            </DropdownButton>
             <div>Filter</div>
           </div>
         </Col>
@@ -52,9 +69,9 @@ const Movies = () => {
           <Row>
             <Col lg={12}>
               <div className='collection-card-list'>{
-              keyword===null?
-              popularMovies.results && popularMovies.results.map((movie) => (<MoviesCollectionCard movie={movie} />)):
-              searchMovie.results && searchMovie.results.map((movie) => (<MoviesCollectionCard movie={movie} />))
+                keyword === null ?
+                  popularMovies.results && popularMovies.results.map((movie) => (<MoviesCollectionCard movie={movie} />)) :
+                  searchMovie.results && searchMovie.results.map((movie) => (<MoviesCollectionCard movie={movie} />))
               }
               </div>
               <div>
