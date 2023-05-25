6ea8d82afc994ed8ff18b7f16cf1f4960a2b42d3
JIS0098 <llqrw55@naver.com>
2023-05-25 19:11:31 +0900

    졍렬 성공
    눈물난다!

--- src/page/Movies.js
+++ src/page/Movies.js
@@ -21,10 +21,10 @@ const API_KEY = process.env.REACT_APP_API_KEY;
 const Movies = () => {
   const { popularMovies, loading, } = useSelector((state) => state.movie);
   const [query, setQuery] = useSearchParams("");
-  const [list, setList] = useState(popularMovies.results);
+  const [list, setList] = useState([]);
   const dispatch = useDispatch();
   console.log("인기", popularMovies);
-  let keyword = query.get("q")
+  let keyword = query.get("q") || ""
 
   const getSearchMovie = async () => {
     let searchMovieApi = await api.get(`/search/movie?api_key=${API_KEY}&language=en-US&page=1&include_adult=false&query=${keyword}`)
@@ -52,14 +52,38 @@ const Movies = () => {
               <Dropdown.ItemText>Sort Results By</Dropdown.ItemText>
               <DropdownButton id="dropdown-item-button" title="Sort By">
                 <Dropdown.Item as="button">None</Dropdown.Item>
-                <Dropdown.Item onClick={() => { let copy = [...list]; copy.sort((a, b) => (b.popularity - a.popularity)); setList(copy); }} as="button">Popularity(Desc)</Dropdown.Item>
-                <Dropdown.Item onClick={() => { let copy = [...list]; copy.sort((a, b) => (a.popularity - b.popularity)); setList(copy); }} as="button">Popularity(Asc)</Dropdown.Item>
-                <Dropdown.Item onClick={() => { let copy = [...list]; copy.sort((a, b) => (b.release_date - a.release_date)); setList(copy); }} as="button">Release Day(Desc)</Dropdown.Item>
-                <Dropdown.Item onClick={() => { let copy = [...list]; copy.sort((a, b) => (a.release_date - b.release_date)); setList(copy); }} as="button">Release Day(Asc)</Dropdown.Item>
-                <Dropdown.Item onClick={() => { let copy = [...list]; copy.sort((a, b) => (b.vote_average - a.vote_average)); setList(copy); }} as="button">Vote(Desc)</Dropdown.Item>
-                <Dropdown.Item onClick={() => { let copy = [...list]; copy.sort((a, b) => (a.vote_average - b.vote_average)); setList(copy); }} as="button">Vote(Asc)</Dropdown.Item>
-                <Dropdown.Item onClick={() => { let copy = [...list]; copy.sort((a, b) => (b.popularity - a.popularity)); setList(copy); }} as="button">Revenue(Desc)</Dropdown.Item>
-                <Dropdown.Item onClick={() => { let copy = [...list]; copy.sort((a, b) => (b.popularity - a.popularity)); setList(copy); }} as="button">Revenue(Asc)</Dropdown.Item>
+                <Dropdown.Item onClick={() => {
+                  let copy = [...list]; copy.sort((a, b) => (b.popularity - a.popularity)); setList(copy);
+                  popularMovies.results.sort((a, b) => (b.popularity - a.popularity))
+                }} as="button">Popularity(Desc)</Dropdown.Item>
+                <Dropdown.Item onClick={() => {
+                  let copy = [...list]; copy.sort((a, b) => (a.popularity - b.popularity)); setList(copy);
+                  popularMovies.results.sort((a, b) => (a.popularity - b.popularity))
+                }} as="button">Popularity(Asc)</Dropdown.Item>
+                <Dropdown.Item onClick={() => {
+                  let copy = [...list]; copy.sort((a, b) => (b.release_date - a.release_date)); setList(copy);
+                  popularMovies.results.sort((a, b) => (b.release_date - a.release_date))
+                }} as="button">Release Day(Desc)</Dropdown.Item>
+                <Dropdown.Item onClick={() => {
+                  let copy = [...list]; copy.sort((a, b) => (a.release_date - b.release_date)); setList(copy);
+                  popularMovies.results.sort((a, b) => (a.release_date - b.release_date))
+                }} as="button">Release Day(Asc)</Dropdown.Item>
+                <Dropdown.Item onClick={() => {
+                  let copy = [...list]; copy.sort((a, b) => (b.vote_average - a.vote_average)); setList(copy);
+                  popularMovies.results.sort((a, b) => (b.vote_average - a.vote_average))
+                }} as="button">Vote(Desc)</Dropdown.Item>
+                <Dropdown.Item onClick={() => {
+                  let copy = [...list]; copy.sort((a, b) => (a.vote_average - b.vote_average)); setList(copy);
+                  popularMovies.results.sort((a, b) => (a.vote_average - b.vote_average))
+                }} as="button">Vote(Asc)</Dropdown.Item>
+                <Dropdown.Item onClick={() => {
+                  let copy = [...list]; copy.sort((a, b) => (b.popularity - a.popularity)); setList(copy);
+                  popularMovies.results.sort((a, b) => (a.popularity - b.popularity))
+                }} as="button">Revenue(Desc)</Dropdown.Item>
+                <Dropdown.Item onClick={() => {
+                  let copy = [...list]; copy.sort((a, b) => (b.popularity - a.popularity)); setList(copy);
+                  popularMovies.results.sort((a, b) => (a.popularity - b.popularity))
+                }} as="button">Revenue(Asc)</Dropdown.Item>
               </DropdownButton>
             </DropdownButton>
             <div>Filter</div>
@@ -69,9 +93,9 @@ const Movies = () => {
           <Row>
             <Col lg={12}>
               <div className='collection-card-list'>{
-                popularMovies?
-                  list && list.map((movie) => (<MoviesCollectionCard movie={movie} />)):
-                  popularMovies.results && popularMovies.results.map((movie) => (<MoviesCollectionCard movie={movie} />))
+                keyword === "" ?
+                  popularMovies.results && popularMovies.results.map((movie) => (<MoviesCollectionCard movie={movie} />)) :
+                  list && list.map((movie) => (<MoviesCollectionCard movie={movie} />))
               }
               </div>
               <div>
